ecde3978200c0460a1745f31ca735b41bd78fa27
andydevs <akanshul97@gmail.com>
2018-06-26 20:57:12 -0400

    Not using README for long description

--- setup.py
+++ setup.py
@@ -9,16 +9,17 @@ Created: 6 - 24 - 2018
 """
 from setuptools import setup
 
-# Read README.md
-with open('README.md', 'r') as file:
-    readme_text = file.read()
+# Long description
+long_description = """
+Method and function handlers that implement common
+behaviors without writing the entire function
+"""
 
 # Create setup function
 setup(name='good-handlers',
-      version='1.0.1',
+      version='1.0.2',
       description='Method and function handlers that implement common behavior',
-      long_description=readme_text,
-      long_description_content_type='text/markdown',
+      long_description=long_description,
       author='Anshul Kharbanda',
       author_email='akanshul97@gmail.com',
       url='https://www.github.org/andydevs/good-handlers',
