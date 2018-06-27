b2e6ed89f14e7b4aa94b28d05eb4a529bb90e3cd
andydevs <akanshul97@gmail.com>
2018-06-26 21:11:20 -0400

    Added README.md back!

--- setup.py
+++ setup.py
@@ -10,16 +10,15 @@ Created: 6 - 24 - 2018
 from setuptools import setup
 
 # Long description
-long_description = """
-Method and function handlers that implement common
-behaviors without writing the entire function
-"""
+with open('README.md', 'r') as file:
+    long_description = file.read()
 
 # Create setup function
 setup(name='good-handlers',
-      version='1.0.2',
+      version='1.0.3',
       description='Method and function handlers that implement common behavior',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Anshul Kharbanda',
       author_email='akanshul97@gmail.com',
       url='https://www.github.org/andydevs/good-handlers',
